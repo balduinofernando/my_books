248a6d03db633671255ba6f2f8fddc05d3dcafb7
Balduino Fernando <bafernando94@gmail.com>
2022-03-06 16:38:28 +0100

    Usercontroller, routes

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,26 @@
 import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import { User } from "../models/User";
 
 class UserController {
-    async create(request: Request, response: Response) {
-        const {name, email, password} = request.body;
-        //console.log(body);
-        return response.send();
+  async create(request: Request, response: Response) {
+    const { name, email, password } = request.body;
+
+    const userRepository = getRepository(User);
+
+    const emailExists = await userRepository.findOne({ email });
+
+    if (emailExists) {
+      return response
+        .status(409)
+        .json({ error: "The informed email has already been taken." });
     }
+
+    const user = userRepository.create({ name, email, password });
+    await userRepository.save(user);
+
+    return response.json({ user });
+  }
 }
 
-export{UserController};
+export { UserController };
--- src/models/User.ts
+++ src/models/User.ts
@@ -11,6 +11,9 @@ class User {
   @Column()
   email: string;
 
+  @Column()
+  password: string;
+
   @CreateDateColumn()
   created_at: Date;
 

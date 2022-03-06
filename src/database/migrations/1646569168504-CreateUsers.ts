c0c96447fe2600791a016fd26475ab476290cfd8
Balduino Fernando <bafernando94@gmail.com>
2022-03-06 13:33:31 +0100

    TypeORM configuration for migrations locations and file type

--- src/database/migrations/1646569168504-CreateUsers.ts
+++ src/database/migrations/1646569168504-CreateUsers.ts
@@ -1,11 +1,39 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { timeStamp } from "console";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateUsers1646569168504 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: "users",
+            columns: [
+                {
+                    name: "id",
+                    type: "uuid",
+                    isPrimary: true
+                }, {
+                    name: "name",
+                    type: "varchar"
+                }, {
+                    name: "email",
+                    type: "varchar"
+                }, {
+                    name: "password",
+                    type: "varchar"
+                },
+                {
+                    name: "created_at",
+                    type: "timestamp"
+                }, {
+                    name: "updated_at",
+                    type: "timestamp",
+                    default: "now()"
+                }]
+        }))
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('users', true)
     }
 
 }
